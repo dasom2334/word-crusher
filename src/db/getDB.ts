aede0cb04340f11b64129d6d3d09ca057ecb4c7e
dasom <dasom2334@gmail.com>
2022-10-06 05:06:05 +0900

    get words to wordDictionary

--- src/db/getDB.ts
+++ src/db/getDB.ts
@@ -21,59 +21,33 @@ import words20 from "./words_length_20";
 import words21 from "./words_length_21";
 import words22 from "./words_length_22";
 
-function getWordFromLength(count: number): string[] {
-  switch (count) {
-    case 1:
-      return words1;
-    case 2:
-      return words2;
-    case 3:
-      return words3;
-    case 4:
-      return words4;
-    case 5:
-      return words5;
-    case 6:
-      return words6;
-    case 7:
-      return words7;
-    case 8:
-      return words8;
-    case 9:
-      return words9;
-    case 10:
-      return words10;
-    case 11:
-      return words11;
-    case 12:
-      return words12;
-    case 13:
-      return words13;
-    case 14:
-      return words14;
-    case 15:
-      return words15;
-    case 16:
-      return words16;
-    case 17:
-      return words17;
-    case 18:
-      return words18;
-    case 19:
-      return words19;
-    case 20:
-      return words20;
-    case 21:
-      return words21;
-    case 22:
-      return words22;
-    default:
-      return words5;
-  }
-}
+const wordDictionary: Record<number, string[]> = {
+  1: words1,
+  2: words2,
+  3: words3,
+  4: words4,
+  5: words5,
+  6: words6,
+  7: words7,
+  8: words8,
+  9: words9,
+  10: words10,
+  11: words11,
+  12: words12,
+  13: words13,
+  14: words14,
+  15: words15,
+  16: words16,
+  17: words17,
+  18: words18,
+  19: words19,
+  20: words20,
+  21: words21,
+  22: words22,
+};
 
 export async function getWords(state: stateProps): Promise<string[]> {
-  return getWordFromLength(state.count)
+  return wordDictionary[state.count]
     .filter((word: string) => {
       const match = word.match(makeRegExpByState(state));
       return match !== null && match[0] === word;
